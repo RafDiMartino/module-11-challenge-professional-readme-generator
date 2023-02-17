2bece7bff46fca78688dc6dc35ef9b54ee1fc1a8
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-17 11:30:16 +0000

    initial-commit project setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();

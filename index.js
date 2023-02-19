fe1ab090a331d086dc26a869f5f32cc03dd8a96b
Raffaele Di Martino <raf@raffaeles-mbp.home>
2023-02-19 16:36:42 +0000

    adding logic to write to file and generate readme on selected folder

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
 const fs = require("fs");
-const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const questions = require("./utils/questions")
 
-
 // function to write README file
-function writeToFile(fileName, data) {
-    const {title, description, installation, usage, license, contributing, questions, tests} = data
+function writeToFile(answers) {
+    fs.mkdir("./generated-files", { recursive: true }, (err) => {if (err) throw err;})
+    fs.writeFile(__dirname + `/generated-files/README.md`, generateMarkdown(answers), (err) => err && console.error(err));
+}   
 
-    console.log(title, description, installation, usage, license, contributing, questions, tests);
-    fs.writeFile("README.md", JSON.stringify(data, null, "\t"), (err) =>err ? console.log(err):console.log("Success!"))
+// async function to initialise the program
+async function init() {
+    try {
+      const answers = await inquirer.prompt(questions);
+      writeToFile(answers)
+    }catch (err) { 
+        throw err;
+    }
 }
-
-// function to initialize program
-function init() {
-    inquirer.prompt(questions)
-        .then((data) => {
-        writeToFile("README.MD", data)
-        const readableData = data
-    })
-}
-
 
 // function call to initialize program
 init();
